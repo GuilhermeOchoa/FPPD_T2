df2b56b14fbb16fae86e2d9c4dfd9b6cd89dca67
Guilherme Ochoa <guilhermeochoa@gmail.com>
2023-11-04 18:45:00 -0300

    WIP- Trabalho 2 de FPPD, módulo Dimex

---
+++ SD/DIMEX/DIMEX-Template.go
@@ -0,0 +1,284 @@
+/*  Construido como parte da disciplina: FPPD - PUCRS - Escola Politecnica
+    Professor: Fernando Dotti  (https://fldotti.github.io/)
+    Modulo representando Algoritmo de Exclusão Mútua Distribuída:
+    Semestre 2023/1
+	Aspectos a observar:
+	   mapeamento de módulo para estrutura
+	   inicializacao
+	   semantica de concorrência: cada evento é atômico
+	   							  módulo trata 1 por vez
+	Q U E S T A O
+	   Além de obviamente entender a estrutura ...
+	   Implementar o núcleo do algoritmo ja descrito, ou seja, o corpo das
+	   funcoes reativas a cada entrada possível:
+	   			handleUponReqEntry()  // recebe do nivel de cima (app)
+				handleUponReqExit()   // recebe do nivel de cima (app)
+				handleUponDeliverRespOk(msgOutro)   // recebe do nivel de baixo
+				handleUponDeliverReqEntry(msgOutro) // recebe do nivel de baixo
+*/
+
+package DIMEX
+
+import (
+	PP2PLink "SD/PP2PLink"
+	"fmt"
+	"strings"
+)
+
+// ------------------------------------------------------------------------------------
+// ------- principais tipos
+// ------------------------------------------------------------------------------------
+
+type State int // enumeracao dos estados possiveis de um processo
+const (
+	noMX State = iota
+	wantMX
+	inMX
+)
+
+type dmxReq int // enumeracao dos estados possiveis de um processo
+const (
+	ENTER dmxReq = iota
+	EXIT
+)
+
+type dmxResp struct { // mensagem do módulo DIMEX informando que pode acessar - pode ser somente um sinal (vazio)
+	// mensagem para aplicacao indicando que pode prosseguir
+}
+
+type DIMEX_Module struct {
+	Req       chan dmxReq  // canal para receber pedidos da aplicacao (REQ e EXIT)
+	Ind       chan dmxResp // canal para informar aplicacao que pode acessar
+	addresses []string     // endereco de todos, na mesma ordem
+	id        int          // identificador do processo - é o indice no array de enderecos acima
+	st        State        // estado deste processo na exclusao mutua distribuida
+	waiting   []bool       // processos aguardando tem flag true
+	lcl       int          // relogio logico local
+	reqTs     int          // timestamp local da ultima requisicao deste processo
+	nbrResps  int
+	dbg       bool
+
+	Pp2plink *PP2PLink.PP2PLink // acesso aa comunicacao enviar por PP2PLinq.Req  e receber por PP2PLinq.Ind
+}
+
+// ------------------------------------------------------------------------------------
+// ------- inicializacao
+// ------------------------------------------------------------------------------------
+
+func NewDIMEX(_addresses []string, _id int, _dbg bool) *DIMEX_Module {
+
+	p2p := PP2PLink.NewPP2PLink(_addresses[_id], _dbg)
+
+	dmx := &DIMEX_Module{
+		Req: make(chan dmxReq, 1),
+		Ind: make(chan dmxResp, 1),
+
+		addresses: _addresses,
+		id:        _id,
+		st:        noMX,
+		waiting:   make([]bool, len(_addresses)),
+		lcl:       0,
+		reqTs:     0,
+		dbg:       _dbg,
+
+		Pp2plink: p2p}
+
+	for i := 0; i < len(dmx.waiting); i++ {
+		dmx.waiting[i] = false
+	}
+	dmx.Start()
+	dmx.outDbg("Init DIMEX!")
+	return dmx
+}
+
+// ------------------------------------------------------------------------------------
+// ------- nucleo do funcionamento
+// ------------------------------------------------------------------------------------
+
+func (module *DIMEX_Module) Start() {
+
+	go func() {
+		for {
+			select {
+			case dmxR := <-module.Req: // vindo da  aplicação
+				if dmxR == ENTER {
+					module.outDbg("app pede mx")
+					module.handleUponReqEntry() // ENTRADA DO ALGORITMO
+
+				} else if dmxR == EXIT {
+					module.outDbg("app libera mx")
+					module.handleUponReqExit() // ENTRADA DO ALGORITMO
+				}
+
+			case msgOutro := <-module.Pp2plink.Ind: // vindo de outro processo
+				//fmt.Printf("dimex recebe da rede: ", msgOutro)
+				if strings.Contains(msgOutro.Message, "respOK") {
+					module.outDbg("         <<<---- responde! " + msgOutro.Message)
+					module.handleUponDeliverRespOk(msgOutro) // ENTRADA DO ALGORITMO
+
+				} else if strings.Contains(msgOutro.Message, "reqEntry") {
+					module.outDbg("          <<<---- pede??  " + msgOutro.Message)
+					module.handleUponDeliverReqEntry(msgOutro) // ENTRADA DO ALGORITMO
+
+				}
+			}
+		}
+	}()
+}
+
+// ------------------------------------------------------------------------------------
+// ------- tratamento de pedidos vindos da aplicacao
+// ------- UPON ENTRY
+// ------- UPON EXIT
+// ------------------------------------------------------------------------------------
+
+func (module *DIMEX_Module) handleUponReqEntry() {
+	/*
+					upon event [ dmx, Entry  |  r ]  do
+		    			lts.ts++
+		    			myTs := lts
+		    			resps := 0
+		    			para todo processo p
+							trigger [ pl , Send | [ reqEntry, r, myTs ]
+		    			estado := queroSC
+	*/
+
+	// Aumenta o carimbo de tempo local
+	module.lcl++
+
+	// Define o carimbo de tempo local da última solicitação deste processo
+	module.reqTs = module.lcl
+
+	// Inicializa o contador de respostas
+	module.nbrResps = 0
+
+	// Para cada processo p
+	for i, addr := range module.addresses {
+		if i == module.id {
+			continue // Não envie solicitação para si mesmo
+		}
+
+		// Envia uma mensagem [reqEntry, r, myTs] para o processo p
+		message := fmt.Sprintf("[reqEntry, %d, %d]", module.id, module.reqTs)
+		module.sendToLink(addr, message, "Entry Request")
+
+		// Atualiza o estado para "wantMX" (quer a exclusão mútua)
+		module.st = wantMX
+	}
+}
+func (module *DIMEX_Module) handleUponReqExit() {
+	/*
+						upon event [ dmx, Exit  |  r  ]  do
+		       				para todo [p, r, ts ] em waiting
+		          				trigger [ pl, Send | p , [ respOk, r ]  ]
+		    				estado := naoQueroSC
+							waiting := {}
+	*/
+	// Para todos os processos em waiting
+	for i := 0; i < len(module.waiting); i++ {
+		if module.waiting[i] {
+			// Envia uma mensagem [respOk, r] para o processo i
+			message := fmt.Sprintf("[respOk, %d]", module.id)
+			module.sendToLink(module.addresses[i], message, "Exit Response")
+		}
+	}
+
+	// Atualiza o estado para "noMX" (não está na exclusão mútua)
+	module.st = noMX
+
+	// Limpa a lista de processos esperando
+	for i := 0; i < len(module.waiting); i++ {
+		module.waiting[i] = false
+	}
+
+}
+
+// ------------------------------------------------------------------------------------
+// ------- tratamento de mensagens de outros processos
+// ------- UPON respOK
+// ------- UPON reqEntry
+// ------------------------------------------------------------------------------------
+
+func (module *DIMEX_Module) handleUponDeliverRespOk(msgOutro PP2PLink.PP2PLink_Ind_Message) {
+	/*
+						upon event [ pl, Deliver | p, [ respOk, r ] ]
+		      				resps++
+		      				se resps = N
+		    				então trigger [ dmx, Deliver | free2Access ]
+		  					    estado := estouNaSC
+
+	*/
+
+	module.nbrResps++
+
+	// Se o número de respostas recebidas for igual ao número de processos (N), então é seguro entrar na seção crítica
+	if module.nbrResps == len(module.addresses) {
+		// Dispara um evento de entrega para a aplicação indicando que pode acessar a seção crítica
+		module.Ind <- dmxResp{}
+
+		// Atualiza o estado para "inMX" (está na exclusão mútua)
+		module.st = inMX
+	}
+}
+
+func (module *DIMEX_Module) handleUponDeliverReqEntry(msgOutro PP2PLink.PP2PLink_Ind_Message) {
+	// outro processo quer entrar na SC
+	/*
+						upon event [ pl, Deliver | p, [ reqEntry, r, rts ]  do
+		     				se (estado == naoQueroSC)   OR
+		        				 (estado == QueroSC AND  myTs >  ts)
+							então  trigger [ pl, Send | p , [ respOk, r ]  ]
+		 					senão
+		        				se (estado == estouNaSC) OR
+		           					 (estado == QueroSC AND  myTs < ts)
+		        				então  postergados := postergados + [p, r ]
+		     					lts.ts := max(lts.ts, rts.ts)
+	*/
+
+	var p, r, ts int
+	// Extrai os valores do conteúdo da mensagem
+	_, err := fmt.Sscanf(msgOutro.Message, "[reqEntry, %d, %d, %d]", &p, &r, &ts)
+	if err != nil {
+		// Manipule o erro conforme necessário
+		return
+	}
+
+	// Se o estado é "noMX" ou "wantMX" e o carimbo de tempo e o identificador deste processo são maiores do que os da mensagem,
+	// envie uma mensagem [respOk, r] de volta para o processo que fez a solicitação
+	if (module.st == noMX || module.st == wantMX) && before(module.id, module.reqTs, p, ts) {
+		//if module.st == noMX {//|| (module.st == wantMX && (module.reqTs, module.id) > (ts,p)) {
+		message := fmt.Sprintf("[respOk, %d]", module.id)
+		module.sendToLink(module.addresses[p], message, "Request Entry Response")
+	} else {
+		// Caso contrário, adicione o processo à lista de processos em espera
+		module.waiting[p] = true
+		module.lcl = max(module.lcl, ts)
+	}
+}
+
+// ------------------------------------------------------------------------------------
+// ------- funcoes de ajuda
+// ------------------------------------------------------------------------------------
+
+func (module *DIMEX_Module) sendToLink(address string, content string, space string) {
+	module.outDbg(space + " ---->>>>   to: " + address + "     msg: " + content)
+	module.Pp2plink.Req <- PP2PLink.PP2PLink_Req_Message{
+		To:      address,
+		Message: content}
+}
+
+func before(oneId, oneTs, othId, othTs int) bool {
+	if oneTs < othTs {
+		return true
+	} else if oneTs > othTs {
+		return false
+	} else {
+		return oneId < othId
+	}
+}
+
+func (module *DIMEX_Module) outDbg(s string) {
+	if module.dbg {
+		fmt.Println(". . . . . . . . . . . . [ DIMEX : " + s + " ]")
+	}
+}
