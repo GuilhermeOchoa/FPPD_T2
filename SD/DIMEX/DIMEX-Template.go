6d0e9c6d8519af98622c2e0ca315f93288e241b6
Vitor Mariano Becker <vmarianobecker@gmail.com>
2023-11-10 18:27:16 -0300

    melhorias

--- SD/DIMEX/DIMEX-Template.go
+++ SD/DIMEX/DIMEX-Template.go
@@ -17,8 +17,6 @@
 				handleUponDeliverReqEntry(msgOutro) // recebe do nivel de baixo
 */
 
-// O código DIMEX-Template.go define um módulo para gerenciar exclusão mútua distribuída (Distributed Mutual Exclusion - DIMEX) em um ambiente de sistemas distribuídos. Este módulo utiliza mensagens e estados para assegurar que apenas um processo por vez possa entrar em uma seção crítica (acesso a um recurso compartilhado).
-
 package DIMEX
 
 import (
@@ -31,37 +29,34 @@ import (
 // ------- principais tipos
 // ------------------------------------------------------------------------------------
 
-// Define um tipo personalizado para os estados possíveis de um processo na exclusão mútua distribuída.
 type State int // enumeracao dos estados possiveis de um processo
 const (
-	noMX   State = iota // não quer entrar na seção crítica (SC)
-	wantMX              // quer entrar na SC
-	inMX                // está na SC
+	noMX State = iota
+	wantMX
+	inMX
 )
 
-// Define um tipo para as requisições que o módulo pode receber.
 type dmxReq int // enumeracao dos estados possiveis de um processo
 const (
-	ENTER dmxReq = iota // solicitação para entrar na SC
-	EXIT                // solicitação para sair da SC
+	ENTER dmxReq = iota
+	EXIT
 )
 
-type dmxResp struct { // mensagem do módulo DIMEX informando que pode acessar - pode ser somente um sinal (vazio)
+type dmxResp struct { // mensagem do módulo DIMEX infrmando que pode acessar - pode ser somente um sinal (vazio)
 	// mensagem para aplicacao indicando que pode prosseguir
 }
 
-// Estrutura principal do módulo DIMEX.
 type DIMEX_Module struct {
-	Req       chan dmxReq  // canal para receber pedidos da aplicação (ENTER e EXIT)
+	Req       chan dmxReq  // canal para receber pedidos da aplicacao (REQ e EXIT)
 	Ind       chan dmxResp // canal para informar aplicacao que pode acessar
-	addresses []string     // endereços de todos os processos, na mesma ordem
+	addresses []string     // endereco de todos, na mesma ordem
 	id        int          // identificador do processo - é o indice no array de enderecos acima
 	st        State        // estado deste processo na exclusao mutua distribuida
 	waiting   []bool       // processos aguardando tem flag true
 	lcl       int          // relogio logico local
 	reqTs     int          // timestamp local da ultima requisicao deste processo
-	nbrResps  int          // contador de respostas recebidas
-	dbg       bool         // flag para depuração
+	nbrResps  int
+	dbg       bool
 
 	Pp2plink *PP2PLink.PP2PLink // acesso aa comunicacao enviar por PP2PLinq.Req  e receber por PP2PLinq.Ind
 }
@@ -70,12 +65,11 @@ type DIMEX_Module struct {
 // ------- inicializacao
 // ------------------------------------------------------------------------------------
 
-// Função para criar uma nova instância do módulo DIMEX.
 func NewDIMEX(_addresses []string, _id int, _dbg bool) *DIMEX_Module {
 
-	p2p := PP2PLink.NewPP2PLink(_addresses[_id], _dbg) // Cria um novo ponto-a-ponto.
+	p2p := PP2PLink.NewPP2PLink(_addresses[_id], _dbg)
 
-	dmx := &DIMEX_Module{ // Inicializa a estrutura do DIMEX.
+	dmx := &DIMEX_Module{
 		Req: make(chan dmxReq, 1),
 		Ind: make(chan dmxResp, 1),
 
@@ -90,42 +84,41 @@ func NewDIMEX(_addresses []string, _id int, _dbg bool) *DIMEX_Module {
 		Pp2plink: p2p}
 
 	for i := 0; i < len(dmx.waiting); i++ {
-		dmx.waiting[i] = false // Inicializa o vetor de espera.
+		dmx.waiting[i] = false
 	}
-	dmx.Start()               // Inicia o módulo.
-	dmx.outDbg("Init DIMEX!") // Mensagem de depuração.
-	return dmx                // Retorna a instância do módulo.
+	dmx.Start()
+	dmx.outDbg("Init DIMEX!")
+	return dmx
 }
 
 // ------------------------------------------------------------------------------------
 // ------- nucleo do funcionamento
 // ------------------------------------------------------------------------------------
 
-// Inicia o loop principal que lida com eventos.
 func (module *DIMEX_Module) Start() {
-	go func() { // Inicia uma nova goroutine.
-		for { // Loop infinito.
-			select { // Espera por um evento.
-			case dmxR := <-module.Req: // Se um evento vier do canal Req.
-				if dmxR == ENTER { // Se for um pedido de entrada na SC.
-					module.outDbg("app pede mx")
-					module.handleUponReqEntry() // Chama a função de tratamento de entrada. ENTRADA DO ALGORITMO
 
-				} else if dmxR == EXIT { // Se for um pedido de saída da SC.
+	go func() {
+		for {
+			select {
+			case dmxR := <-module.Req: // vindo da  aplicação
+				if dmxR == ENTER {
+					module.outDbg("app pede mx")
+					module.handleUponReqEntry() // ENTRADA DO ALGORITMO
+
+				} else if dmxR == EXIT {
 					module.outDbg("app libera mx")
-					module.handleUponReqExit() // Chama a função de tratamento de saída. ENTRADA DO ALGORITMO
+					module.handleUponReqExit() // ENTRADA DO ALGORITMO
 				}
 
-			case msgOutro := <-module.Pp2plink.Ind: // Se um evento vier de outro processo.
-				//fmt.Printf("dimex recebe da rede: ", msgOutro)
-				// Avalia o conteúdo da mensagem recebida e chama a função de tratamento correspondente.
+			case msgOutro := <-module.Pp2plink.Ind: // vindo de outro processo
+				fmt.Printf("dimex recebe da rede: ", msgOutro)
 				if strings.Contains(msgOutro.Message, "respOK") {
 					module.outDbg("         <<<---- responde! " + msgOutro.Message)
-					module.handleUponDeliverRespOk(msgOutro) // Trata uma resposta de OK. ENTRADA DO ALGORITMO
+					module.handleUponDeliverRespOk(msgOutro) // ENTRADA DO ALGORITMO
 
 				} else if strings.Contains(msgOutro.Message, "reqEntry") {
 					module.outDbg("          <<<---- pede??  " + msgOutro.Message)
-					module.handleUponDeliverReqEntry(msgOutro) // Trata uma solicitação de entrada. ENTRADA DO ALGORITMO
+					module.handleUponDeliverReqEntry(msgOutro) // ENTRADA DO ALGORITMO
 
 				}
 			}
@@ -150,25 +143,20 @@ func (module *DIMEX_Module) handleUponReqEntry() {
 		    			estado := queroSC
 	*/
 
-	// Aumenta o carimbo de tempo local
 	module.lcl++
-	// Define o time stamp local da última solicitação deste processo
 	module.reqTs = module.lcl
-	// Inicializa o contador de respostas
 	module.nbrResps = 0
-	// Para cada processo p
+
 	for i, addr := range module.addresses {
 		if i == module.id {
-			//module.outDbg("Não envia para si mesmo: " + addr)
-			continue // Não envie solicitação para si mesmo
+			continue
 		}
-		// Envia uma mensagem [reqEntry, r, myTs] para o processo p
 		message := fmt.Sprintf("reqEntry, %d, %d", module.id, module.reqTs)
 		module.sendToLink(addr, message, "Request Entry")
-		// Atualiza o estado para "wantMX" (quer a exclusão mútua)
-		module.st = wantMX
 	}
+	module.st = wantMX
 }
+
 func (module *DIMEX_Module) handleUponReqExit() {
 	/*
 						upon event [ dmx, Exit  |  r  ]  do
@@ -177,24 +165,17 @@ func (module *DIMEX_Module) handleUponReqExit() {
 		    				estado := naoQueroSC
 							waiting := {}
 	*/
-	// Para todos os processos em waiting
 	for i := 0; i < len(module.waiting); i++ {
 		if module.waiting[i] {
-			// Envia uma mensagem [respOk, r] para o processo i
 			message := fmt.Sprintf("respOK, %d", module.id)
-			//message := fmt.Sprintf("respOK")
 			module.sendToLink(module.addresses[i], message, "Exit Response")
 		}
 	}
-
-	// Atualiza o estado para "noMX" (não está na exclusão mútua)
 	module.st = noMX
 
-	// Limpa a lista de processos esperando
 	for i := 0; i < len(module.waiting); i++ {
 		module.waiting[i] = false
 	}
-
 }
 
 // ------------------------------------------------------------------------------------
@@ -212,19 +193,14 @@ func (module *DIMEX_Module) handleUponDeliverRespOk(msgOutro PP2PLink.PP2PLink_I
 		  					    estado := estouNaSC
 
 	*/
-	//module.outDbg(msgOutro.Message)
-	//module.outDbg("entrou no handleUponDeliverRespOk")
-	//atualiza o contador de respostas
 	module.nbrResps++
 	if module.nbrResps == len(module.addresses)-1 {
 		module.Ind <- dmxResp{}
 		module.st = inMX
 	}
-
 }
 
 func (module *DIMEX_Module) handleUponDeliverReqEntry(msgOutro PP2PLink.PP2PLink_Ind_Message) {
-	// outro processo quer entrar na SC
 	/*
 						upon event [ pl, Deliver | p, [ reqEntry, r, rts ]  do
 		     				se (estado == naoQueroSC)   OR
@@ -236,35 +212,28 @@ func (module *DIMEX_Module) handleUponDeliverReqEntry(msgOutro PP2PLink.PP2PLink
 		        				então  postergados := postergados + [p, r ]
 		     					lts.ts := max(lts.ts, rts.ts)
 	*/
-	//module.outDbg(msgOutro.Message)
 	var p, ts int
 	_, err := fmt.Sscanf(msgOutro.Message, "reqEntry, %d, %d", &p, &ts)
 	if err != nil {
 		// Manipule o erro conforme necessário
 		return
 	}
-	//module.outDbg(fmt.Sprintf("p: %d, ts: %d", p, ts))
-	//module.outDbg(fmt.Sprintf("module.st: %d, module.reqTs: %d", module.st, module.reqTs))
-	//
+
+	//if (module.st == noMX) || (module.st == wantMX && !before(module.id, module.reqTs, p, ts)) {
 	//if module.st == noMX || (module.st == wantMX && (module.reqTs > ts || (module.reqTs == ts && module.id > p))) {
-	if (module.st == noMX) || (module.st == wantMX && before(module.id, module.reqTs, p, ts)) {
+	if (module.st == noMX) || (module.st == wantMX && before(p, ts, module.id, module.reqTs)) {
 		message := fmt.Sprintf("respOK, %d", module.id)
 		module.sendToLink(module.addresses[p], message, "Request Entry Response")
 	} else {
-		//if module.st == inMX || (module.st == wantMX && before(p, ts, module.id, module.reqTs)) {
-		//module.outDbg("Adicionando processo na lista de espera: " + msgOutro.Message)
 		module.waiting[p] = true
-		//module.outDbg(fmt.Sprintf("module.waiting: %v", module.waiting))
-		module.lcl = max(module.lcl, ts)
-		//module.outDbg(fmt.Sprintf("module.lcl: %d", module.lcl))
 	}
+	module.lcl = max(module.lcl, ts)
 }
 
 // ------------------------------------------------------------------------------------
 // ------- funcoes de ajuda
 // ------------------------------------------------------------------------------------
 
-// Envia uma mensagem para um endereço específico.
 func (module *DIMEX_Module) sendToLink(address string, content string, space string) {
 	module.outDbg(space + " ---->>>>   to: " + address + "     msg: " + content)
 	module.Pp2plink.Req <- PP2PLink.PP2PLink_Req_Message{
@@ -272,7 +241,6 @@ func (module *DIMEX_Module) sendToLink(address string, content string, space str
 		Message: content}
 }
 
-// Compara dois pares de identificador e timestamp para determinar a precedência.
 func before(oneId, oneTs, othId, othTs int) bool {
 	if oneTs < othTs {
 		return true
@@ -283,7 +251,6 @@ func before(oneId, oneTs, othId, othTs int) bool {
 	}
 }
 
-// Imprime mensagens de depuração se o modo de depuração estiver habilitado.
 func (module *DIMEX_Module) outDbg(s string) {
 	if module.dbg {
 		fmt.Println(". . . . . . . . . . . . [ DIMEX : " + s + " ]")
--- SD/useDIMEX-f.go
+++ SD/useDIMEX-f.go
@@ -1,9 +1,9 @@
 // Construido como parte da disciplina: Sistemas Distribuidos - PUCRS - Escola Politecnica
 //  Professor: Fernando Dotti  (https://fldotti.github.io/)
 // Uso p exemplo:
-//   go run useDIMEX-f.go 0 127.0.0.1:5000  127.0.0.1:6001  127.0.0.1:7002 ")
-//   go run useDIMEX-f.go 1 127.0.0.1:5000  127.0.0.1:6001  127.0.0.1:7002 ")
-//   go run useDIMEX-f.go 2 127.0.0.1:5000  127.0.0.1:6001  127.0.0.1:7002 ")
+//   go run usaDIMEX.go 0 127.0.0.1:5000  127.0.0.1:6001  127.0.0.1:7002 ")
+//   go run usaDIMEX.go 1 127.0.0.1:5000  127.0.0.1:6001  127.0.0.1:7002 ")
+//   go run usaDIMEX.go 2 127.0.0.1:5000  127.0.0.1:6001  127.0.0.1:7002 ")
 // ----------
 // LANCAR N PROCESSOS EM SHELL's DIFERENTES, UMA PARA CADA PROCESSO.
 // para cada processo fornecer: seu id único (0, 1, 2 ...) e a mesma lista de processos.
@@ -29,17 +29,16 @@
 package main
 
 import (
-	"SD/DIMEX" // Importa o pacote DIMEX que contém a implementação do protocolo de exclusão mútua.
-	"fmt"      // Importa o pacote fmt para realizar operações de I/O formatadas, como imprimir na tela.
-	"os"       // Importa o pacote fmt para realizar operações de I/O formatadas, como imprimir na tela.
-	"strconv"  // Importa o pacote strconv para converter strings para outros tipos de dados.
-	"time"     // Importa o pacote time para manipular operações relacionadas a tempo, como esperas.F
+	"SD/DIMEX"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
 
-	if len(os.Args) < 2 { // Verifica se o número de argumentos de linha de comando é menor que 2 (o que significa que falta algum argumento).
-		// Se faltar argumentos, imprime as instruções de como executar o programa corretamente.
+	if len(os.Args) < 2 {
 		fmt.Println("Please specify at least one address:port!")
 		fmt.Println("go run useDIMEX-f.go 0 127.0.0.1:5000  127.0.0.1:6001  127.0.0.1:7002 ")
 		fmt.Println("go run useDIMEX-f.go 1 127.0.0.1:5000  127.0.0.1:6001  127.0.0.1:7002 ")
@@ -47,53 +46,48 @@ func main() {
 		return
 	}
 
-	id, _ := strconv.Atoi(os.Args[1]) // Converte o segundo argumento de linha de comando para um inteiro, que representa o ID do processo.
-	addresses := os.Args[2:]          // Armazena todos os argumentos após o segundo em um slice, que são os endereços dos outros processos.
-	fmt.Print("id: ", id, "   ")      // Imprime o ID do processo.
-	fmt.Println(addresses)            // Imprime os endereços dos outros processos.
+	id, _ := strconv.Atoi(os.Args[1])
+	addresses := os.Args[2:]
+	// fmt.Print("id: ", id, "   ") fmt.Println(addresses)
 
-	// Cria uma nova instância do módulo DIMEX com os endereços fornecidos e o ID.
 	var dmx *DIMEX.DIMEX_Module = DIMEX.NewDIMEX(addresses, id, true)
-	//fmt.Println(dmx)
+	fmt.Println(dmx)
 
 	// abre arquivo que TODOS processos devem poder usar
-	// Tenta abrir o arquivo mxOUT.txt para adicionar conteúdo ao final.
-	file, err := os.OpenFile("./mxOUT.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile("./mxOUT10.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		fmt.Println("Error opening file:", err) // Se houver erro ao abrir o arquivo, imprime a mensagem de erro.
-		return                                  // Encerra o programa se não puder abrir o arquivo.
+		fmt.Println("Error opening file:", err)
+		return
 	}
-	defer file.Close() // Garante que o arquivo será fechado no final da execução da função main.
+	defer file.Close() // Ensure the file is closed at the end of the function
 
 	// espera para facilitar inicializacao de todos processos (a mao)
-	time.Sleep(3 * time.Second)
+	time.Sleep(5 * time.Second)
 
-	// Inicia um loop infinito.
 	for {
 		// SOLICITA ACESSO AO DIMEX
-		fmt.Println("[ APP id: ", id, " PEDE   MX ]") // Imprime que o processo está pedindo acesso exclusivo (MX).
-		dmx.Req <- DIMEX.ENTER                        // Envia uma mensagem para o canal Req do módulo DIMEX para entrar na seção crítica.
-		fmt.Println("[ APP id: ", id, " ESPERA MX ]") // Imprime que o processo está esperando a liberação do acesso exclusivo.
+		fmt.Println("[ APP id: ", id, " PEDE   MX ]")
+		dmx.Req <- DIMEX.ENTER
+		fmt.Println("[ APP id: ", id, " ESPERA MX ]")
 		// ESPERA LIBERACAO DO MODULO DIMEX
-		<-dmx.Ind                                     // Espera receber uma mensagem no canal Ind, que indica que o acesso foi concedido.
-		fmt.Println("[ APP id: ", id, " RECEBE MX ]") // Imprime que o processo recebeu acesso exclusivo.
+		<-dmx.Ind //
 		// A PARTIR DAQUI ESTA ACESSANDO O ARQUIVO SOZINHO
-		_, err = file.WriteString("|") // Escreve o caractere '|' no arquivo, que marca a entrada na seção crítica.
+		_, err = file.WriteString("|") // marca entrada no arquivo
 		if err != nil {
-			fmt.Println("Error writing to file:", err) // Se houver erro ao escrever no arquivo, imprime a mensagem de erro.
-			return                                     // Encerra o programa se não puder escrever no arquivo.
+			fmt.Println("Error writing to file:", err)
+			return
 		}
 
-		fmt.Println("[ APP id: ", id, " *EM*   MX ]") // Imprime que o processo está dentro da seção crítica.
+		fmt.Println("[ APP id: ", id, " *EM*   MX ]")
 
-		_, err = file.WriteString(".") // Escreve o caractere '.' no arquivo, que marca a saída da seção crítica.
+		_, err = file.WriteString(".") // marca saida no arquivo
 		if err != nil {
-			fmt.Println("Error writing to file:", err) // Se houver erro ao escrever no arquivo, imprime a mensagem de erro.
-			return                                     // Encerra o programa se não puder escrever no arquivo.
+			fmt.Println("Error writing to file:", err)
+			return
 		}
 
 		// AGORA VAI LIBERAR O ARQUIVO PARA OUTROS
-		dmx.Req <- DIMEX.EXIT                         // Envia uma mensagem para o canal Req do módulo DIMEX para sair da seção crítica.
-		fmt.Println("[ APP id: ", id, " FORA   MX ]") // Imprime que o processo saiu da seção crítica.
+		dmx.Req <- DIMEX.EXIT //
+		fmt.Println("[ APP id: ", id, " FORA   MX ]")
 	}
 }
